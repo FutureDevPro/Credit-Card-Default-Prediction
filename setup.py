967c313826b6e6414ccbbf5267b15383a40513e1
FutureDevPro <udit27officials@gmail.com>
2026-05-02 00:31:39 +0530

    fixx

--- setup.py
+++ setup.py
@@ -1,9 +1,2 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="credit-card-fraud",
-    version="0.1.0",
-    description="Credit card fraud detection",
-    packages=find_packages(),
-    python_requires=">=3.8",
-)
